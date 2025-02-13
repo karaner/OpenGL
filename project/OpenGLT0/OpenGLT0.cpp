06c72d3207a505a720c49cf699187857680c3cb1
wenyanhao <wenyanhao2011@163.com>
2025-02-13 14:25:13 +0800

    1、配置freeglut和glew工程 2、可以运行进行基本的api测试

---
+++ project/OpenGLT0/OpenGLT0.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <iostream>
+#include <GL/freeglut.h>//freeGLUT窗口库
+
+/**
+ * 渲染回调函数
+ */
+void RenderScenceCB() {
+    // 清空颜色缓存
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // 获取 OpenGL 版本信息
+    const char* version = (const char*)glGetString(GL_VERSION);
+    std::cout << "OpenGL Version: " << version << std::endl;
+
+    // 交换前后缓存
+    glutSwapBuffers();
+}
+
+/**
+ * 主函数
+ */
+int main(int argc, char** argv) {
+
+    // 初始化GLUT
+    glutInit(&argc, argv);
+
+    // 显示模式：双缓冲、RGBA
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+    // 窗口设置
+    glutInitWindowSize(480, 320);      // 窗口尺寸
+    glutInitWindowPosition(100, 100);  // 窗口位置
+    glutCreateWindow("Tutorial 01");   // 窗口标题
+
+    // 开始渲染
+    glutDisplayFunc(RenderScenceCB);
+
+    // 缓存清空后的颜色值
+    glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
+
+    // 通知开始GLUT的内部循环
+    glutMainLoop();
+
+    return 0;
+}
+
+
+
+
+
